901c5ec9f95fe2dde65850dcabd6de2bdbcafdfa
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-14 23:00:41 +0300

    Показывает вкус

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    pass
+import asyncio
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get('/taste')
+async def websockets_connect():
+    await asyncio.sleep(1)
+    return JSONResponse({'delicious': True})
 
 
 if __name__ == '__main__':
-    main()
+    uvicorn.run('main:app', port=22020)
